fea226436b73068523387eeeea142255169ad3a5
liuxiong332 <liuxiong332@163.com>
2014-12-28 17:00:03 +0800

    add comment object in article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'root', password: 'root', except: [:index, :show]
   def index
     @articles = Article.all
   end
